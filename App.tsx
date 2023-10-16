176145bf75171ca77d95c7868f4f715927da5a90
Phillip Friis-Alstrup <phillip.friis.alstrup@gmail.com>
2023-10-16 13:36:22 +0200

    Hide splash_screen on render

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigator from './src/navigation/StackNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App(): JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <StackNavigator />
